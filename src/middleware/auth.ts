e4715ec97223a8adbbab23a4bc0c794b5b412fe9
Ross Llewellyn <rossllewellyn@Rosss-MacBook-Pro-4.local>
2023-04-24 18:50:46 +0100

    auth fix

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -2,16 +2,22 @@ import { MiddlewareFn } from "type-graphql";
 import Context from "../graphql/context";
 import jwt from "jsonwebtoken";
 import config from "../config";
+import { ApolloError } from "apollo-server-express";
 
 const auth: MiddlewareFn<Context> = ({ context }, next) => {
   const authHeader = context.req.headers?.authorization;
-  
-  const token = authHeader.split(" ")[1];
-  const decodedToken = jwt.verify(token, config.auth.secret);
-  // @ts-ignore
-  context.userId = decodedToken.userId;
 
-  return next();
+  if (!authHeader) throw new ApolloError("No token", "401");
+  
+  try {
+    const token = authHeader.split(" ")[1];
+    const decodedToken = jwt.verify(token, config.auth.secret) as any;
+    context.userId = decodedToken.userId;
+
+    return next();
+  } catch (error) {
+    throw new ApolloError("Not authorised", "401");
+  }
 }
 
 export default auth;
